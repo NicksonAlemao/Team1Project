fdbba504fee7de0b09ff9e90545b0b96a51f98a5
NicksonAlemao <nicksonalemao@hotmail.com>
2020-11-11 15:20:38 +0000

    development branch created and added new initial files

---
+++ src/main/java/net/atos/helpqueue/HelpQueueApplication.java
@@ -0,0 +1,13 @@
+package net.atos.helpqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpQueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpQueueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/atos/helpqueue/HelpQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package net.atos.helpqueue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpQueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
