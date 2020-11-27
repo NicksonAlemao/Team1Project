5f57c2e78ce180e454287743bf68015e9807aa76
NicksonAlemao <nicksonalemao@hotmail.com>
2020-11-27 14:45:49 +0000

    css style issue for tickets

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,54 +1,65 @@
 "use strict";
 
 const output = document.getElementById("activeTickets");
+function renderTickets() {
+    fetch("/read")
+        .then(function (response) {
+            return response.json();
+        }).then(function (tickets) {
+            output.innerHTML = "";
+            for (let ticket of tickets) {
+                // create card
+                const newTicket = document.createElement("div");
+                newTicket.className = "card grid-active";
+                // create card wrapper
+                const card = document.createElement("div");
+                card.className = "card-body card-padding";
+                // create card title and append
+                const title = document.createElement("h5");
+                title.className = "card-title";
+                title.innerText = ticket.problemTitle;
+                card.appendChild(title);
+                // create card subtitle and append
+                const time = document.createElement("h6");
+                time.className = "card-subtitle mb-2 text-muted";
+                time.innerText = ticket.ticketCreationTime;
+                card.appendChild(time);
+                // create card description and append
+                const desc = document.createElement("p");
+                desc.className = "card-text";
+                desc.innerText = ticket.problemDescription;
+                card.appendChild(desc);
+                // create card upvote count and append
+                const votes = document.createElement("h6");
+                votes.className = "card-subtitle mb-2 text-muted";
+                votes.innerText = "UpVotes: " + ticket.upVotes;
+                card.appendChild(votes);
+                // add edit button and upvote button
+                const edit = document.createElement("a");
+                edit.className = "card-link";
+                edit.href = "#";
+                edit.innerText = "Edit";
+                card.appendChild(edit);
+                const vote = document.createElement("a");
+                vote.className = "card-link";
+                vote.href = "#";
+                vote.innerText = "UpVote";
+                vote.addEventListener("click", function () {
+                    addUpVote(ticket.id);
+                })
+                card.appendChild(vote);
+                // append whole card to the container
+                newTicket.appendChild(card);
+                output.appendChild(newTicket);
+            }
 
-fetch("/read")
-    .then(function (response) {
-        return response.json();
-    }).then(function (tickets) {
+            //console.log("Ticket: ", tickets);
+        }).catch(function (error) {
+            console.log(error);
+        });
+}
 
-        for (let ticket of tickets) {
-            // create card
-            const newTicket = document.createElement("div");
-            newTicket.className = "card grid-active";
-            // create card wrapper
-            const card = document.createElement("div");
-            card.className = "card-body card-padding";
-            // create card title and append
-            const title = document.createElement("h5");
-            title.className = "card-title";
-            title.innerText = ticket.problemTitle;
-            card.appendChild(title);
-            // create card subtitle and append
-            const time = document.createElement("h6");
-            time.className = "card-subtitle mb-2 text-muted";
-            time.innerText = ticket.ticketCreationTime;
-            card.appendChild(time);
-            // create card description and append
-            const desc = document.createElement("p");
-            desc.className = "card-text";
-            desc.innerText = ticket.problemDescription;
-            card.appendChild(desc);
-            // add edit button and upvote button
-            const edit = document.createElement("a");
-            edit.className = "card-link";
-            edit.href = "#";
-            edit.innerText = "Edit";
-            card.appendChild(edit);
-            const vote = document.createElement("a");
-            vote.className = "card-link";
-            vote.href = "#";
-            vote.innerText = "Upvote";
-            card.appendChild(vote);
-            // append whole card to the container
-            newTicket.appendChild(card);
-            output.appendChild(newTicket);
-        }
-
-        //console.log("Ticket: ", tickets);
-    }).catch(function (error) {
-        console.log(error);
-    });
+renderTickets();
 
 
 const form = document.getElementById("createTicket")
@@ -83,3 +94,13 @@ form.addEventListener("submit", function (event) {
             console.error(error);
         })
 })
+
+function addUpVote(id) {
+    fetch("/upVote/" + id, {
+        method: "PATCH"
+    }).then(response => {
+        console.log(response);
+        renderTickets();
+    })
+        .catch(error => console.error(error));
+}
